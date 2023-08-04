6f7f5d513021308903fb4fc47245f97e7bb2e22f
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-08-04 12:27:57 +0600

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,13 @@
 import mongoose from 'mongoose'
+import env from './env'
 mongoose.set('strictQuery', false)
 
-const mongoUrl = process.env.DB_URL
-const mongoPass = process.env.DB_PASS
-
-if (mongoUrl && mongoPass) {
-  const uri = mongoUrl.replace('<password>', mongoPass)
-  mongoose
-    .connect(uri)
-    .then(() => {
-      console.log('>>>', 'MongoDB connected successfully...')
-    })
-    .catch(() => {
-      console.error('!!!', 'MongoDB connection failed...')
-    })
-} else {
-  console.error('!!!', 'MongoDB env variable missing...')
-}
+const uri = env.DB_URL.replace('<password>', env.DB_PASS)
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log('>>>', 'MongoDB connected successfully...')
+  })
+  .catch(() => {
+    console.error('!!!', 'MongoDB connection failed...')
+  })
